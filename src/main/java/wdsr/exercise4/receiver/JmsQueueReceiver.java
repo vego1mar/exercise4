6e35d672cde9f02c95322637c1a8aa3be66b2824
Paweł Siciński <koryfeusz.ksiezyca@gmail.com>
2017-04-24 21:09:01 +0200

    An implementation of interface for sending Order object, TextMessage and MapMessage to the JMS Broker. Trying to register callback.

--- src/main/java/wdsr/exercise4/receiver/JmsQueueReceiver.java
+++ src/main/java/wdsr/exercise4/receiver/JmsQueueReceiver.java
@@ -1,59 +1,143 @@
 package wdsr.exercise4.receiver;
 
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import wdsr.exercise4.sender.JmsSender;
+import wdsr.exercise4.PriceAlert;
 
 /**
- * TODO Complete this class so that it consumes messages from the given queue and invokes the registered callback when an alert is received.
+ * TODO Complete this class so that it consumes messages from the given queue and invokes the
+ * registered callback when an alert is received.
  * 
  * Assume the ActiveMQ broker is running on tcp://localhost:62616
  */
 public class JmsQueueReceiver {
-	private static final Logger log = LoggerFactory.getLogger(JmsQueueReceiver.class);
-	
-	/**
-	 * Creates this object
-	 * @param queueName Name of the queue to consume messages from.
-	 */
-	public JmsQueueReceiver(final String queueName) {
-		// TODO
-	}
+    private static final Logger log = LoggerFactory.getLogger(JmsQueueReceiver.class);
+    private final String jmsBrokerURL = "tcp://localhost:62616";
+    private final String queueName;
+    private final boolean isJMSSessionTransacted = false;
+    private final int jmsAcknowledgementMode = Session.AUTO_ACKNOWLEDGE;
+    private MessageConsumer consumer;
+    private Session session;
+    private Connection connection;
 
-	/**
-	 * Registers the provided callback. The callback will be invoked when a price or volume alert is consumed from the queue.
-	 * @param alertService Callback to be registered.
-	 */
-	public void registerCallback(AlertService alertService) {
-		// TODO
-	}
-	
-	/**
-	 * Deregisters all consumers and closes the connection to JMS broker.
-	 */
-	public void shutdown() {
-		// TODO
-	}
+    /**
+     * Creates this object
+     * 
+     * @param queueName Name of the queue to consume messages from.
+     */
+    public JmsQueueReceiver(final String queueName) {
+        this.queueName = queueName;
+    }
 
-	// TODO
-	// This object should start consuming messages when registerCallback method is invoked.
-	
-	// This object should consume two types of messages:
-	// 1. Price alert - identified by header JMSType=PriceAlert - should invoke AlertService::processPriceAlert
-	// 2. Volume alert - identified by header JMSType=VolumeAlert - should invoke AlertService::processVolumeAlert
-	// Use different message listeners for and a JMS selector 
-	
-	// Each alert can come as either an ObjectMessage (with payload being an instance of PriceAlert or VolumeAlert class)
-	// or as a TextMessage.
-	// Text for PriceAlert looks as follows:
-	//		Timestamp=<long value>
-	//		Stock=<String value>
-	//		Price=<long value>
-	// Text for VolumeAlert looks as follows:
-	//		Timestamp=<long value>
-	//		Stock=<String value>
-	//		Volume=<long value>
-	
-	// When shutdown() method is invoked on this object it should remove the listeners and close open connection to the broker.   
+    /**
+     * Registers the provided callback. The callback will be invoked when a price or volume alert is
+     * consumed from the queue.
+     * 
+     * @param alertService Callback to be registered.
+     */
+    public void registerCallback(AlertService alertService) {
+        try {
+            ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(jmsBrokerURL);
+            connection = factory.createConnection();
+            connection.start();
+            session = connection.createSession(isJMSSessionTransacted, jmsAcknowledgementMode);
+            Destination destination = session.createQueue(queueName);
+            consumer = session.createConsumer(destination);
+
+            consumer.setMessageListener(new MessageListener() {
+                @Override
+                public void onMessage(Message message) {
+                    try {
+                        switch (message.getJMSType()) {
+                            case "PriceAlert":
+                                alertPrice(alertService, message);
+                                break;
+                            case "VolumeAlert":
+                                break;
+                        }
+                    } catch (JMSException x) {
+                        log.error(x.getMessage() + " [registerCallback::onMessage]");
+                    }
+                }
+            });
+
+        } catch (Exception x) {
+            log.error(x.getMessage() + " [registerCallback]");
+        }
+    }
+
+    /**
+     * Deregisters all consumers and closes the connection to JMS broker.
+     */
+    public void shutdown() {
+        try {
+            consumer.setMessageListener(null);
+            consumer.close();
+            session.close();
+            connection.close();
+        } catch (Exception x) {
+            log.error(x.getMessage());
+        }
+    }
+
+
+    private void alertPrice(AlertService alertService, Message message) {
+        if (message instanceof ObjectMessage) {
+            try {
+                ObjectMessage objectMessage = (ObjectMessage) message;
+                PriceAlert priceAlert = (PriceAlert) objectMessage.getObject();
+                alertService.processPriceAlert(priceAlert);
+                log.info("[registerListenerForPriceAlert::onMessage::ObjectMessage]");
+            } catch (Exception x) {
+                log.error(x.getMessage() + " [registerListenerForPriceAlert::onMessage::ObjectMessage]");
+            }
+
+        } else if (message instanceof TextMessage) {
+            try {
+                TextMessage textMessage = (TextMessage) message;
+                PriceAlert priceAlert = (PriceAlert) textMessage.getBody(PriceAlert.class);
+                alertService.processPriceAlert(priceAlert);
+                log.info("[registerListenerForPriceAlert::onMessage::TextMessage]");
+            } catch (Exception x) {
+                log.error(x.getMessage() + " [registerListenerForPriceAlert::onMessage::TextMessage]");
+            }
+        }
+    }
+
+    // TODO
+    // This object should start consuming messages when registerCallback method is invoked.
+
+    // This object should consume two types of messages:
+    // 1. Price alert - identified by header JMSType=PriceAlert - should invoke
+    // AlertService::processPriceAlert
+    // 2. Volume alert - identified by header JMSType=VolumeAlert - should invoke
+    // AlertService::processVolumeAlert
+    // Use different message listeners for and a JMS selector
+
+    // Each alert can come as either an ObjectMessage (with payload being an instance of PriceAlert
+    // or VolumeAlert class)
+    // or as a TextMessage.
+    // Text for PriceAlert looks as follows:
+    // Timestamp=<long value>
+    // Stock=<String value>
+    // Price=<long value>
+    // Text for VolumeAlert looks as follows:
+    // Timestamp=<long value>
+    // Stock=<String value>
+    // Volume=<long value>
+
+    // When shutdown() method is invoked on this object it should remove the listeners and close
+    // open connection to the broker.
 }
--- src/main/java/wdsr/exercise4/sender/JmsSender.java
+++ src/main/java/wdsr/exercise4/sender/JmsSender.java
@@ -3,43 +3,115 @@ package wdsr.exercise4.sender;
 import java.math.BigDecimal;
 import java.util.Map;
 
+import javax.jms.Connection;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.MapMessage;
+import javax.jms.MessageProducer;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import wdsr.exercise4.Order;
+
 public class JmsSender {
-	private static final Logger log = LoggerFactory.getLogger(JmsSender.class);
-	
-	private final String queueName;
-	private final String topicName;
+    private static final Logger log = LoggerFactory.getLogger(JmsSender.class);
 
-	public JmsSender(final String queueName, final String topicName) {
-		this.queueName = queueName;
-		this.topicName = topicName;
-	}
+    private final String queueName;
+    private final String topicName;
+    private final String jmsBrokerURL = "tcp://localhost:62616";
+    private final int jmsDeliveryMode = DeliveryMode.NON_PERSISTENT;
+    private final boolean isJMSSessionTransacted = false;
+    private final int jmsAcknowledgmentMode = Session.AUTO_ACKNOWLEDGE;
 
-	/**
-	 * This method creates an Order message with the given parameters and sends it as an ObjectMessage to the queue.
-	 * @param orderId ID of the product
-	 * @param product Name of the product
-	 * @param price Price of the product
-	 */
-	public void sendOrderToQueue(final int orderId, final String product, final BigDecimal price) {
-		// TODO
-	}
+    public JmsSender(final String queueName, final String topicName) {
+        this.queueName = queueName;
+        this.topicName = topicName;
+    }
 
-	/**
-	 * This method sends the given String to the queue as a TextMessage.
-	 * @param text String to be sent
-	 */
-	public void sendTextToQueue(String text) {
-		// TODO
-	}
+    /**
+     * This method creates an Order message with the given parameters and sends it as an
+     * ObjectMessage to the queue.
+     * 
+     * @param orderId ID of the product
+     * @param product Name of the product
+     * @param price Price of the product
+     */
+    public void sendOrderToQueue(final int orderId, final String product, final BigDecimal price) {
+        try {
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(jmsBrokerURL);
+            Connection connection = connectionFactory.createConnection();
+            connection.start();
+            Session session = connection.createSession(isJMSSessionTransacted, jmsAcknowledgmentMode);
+            Destination destination = session.createQueue(queueName);
+            MessageProducer producer = session.createProducer(destination);
+            producer.setDeliveryMode(jmsDeliveryMode);
+            Order order = new Order(orderId, product, price);
+            ObjectMessage message = session.createObjectMessage(order);
+            producer.send(message);
+            session.close();
+            connection.close();
+            log.info("sendOrderToQueue( " + orderId + ", " + product + ", " + price + " )");
+        } catch (Exception x) {
+            log.error(x.getMessage());
+        }
+    }
 
-	/**
-	 * Sends key-value pairs from the given map to the topic as a MapMessage.
-	 * @param map Map of key-value pairs to be sent.
-	 */
-	public void sendMapToTopic(Map<String, String> map) {
-		// TODO
-	}
+    /**
+     * This method sends the given String to the queue as a TextMessage.
+     * 
+     * @param text String to be sent
+     */
+    public void sendTextToQueue(String text) {
+        try {
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(jmsBrokerURL);
+            Connection connection = connectionFactory.createConnection();
+            connection.start();
+            Session session = connection.createSession(isJMSSessionTransacted, jmsAcknowledgmentMode);
+            Destination destination = session.createQueue(queueName);
+            MessageProducer producer = session.createProducer(destination);
+            producer.setDeliveryMode(jmsDeliveryMode);
+            TextMessage message = session.createTextMessage(text);
+            producer.send(message);
+            session.close();
+            connection.close();
+            log.info("sendTextToQueue( " + text + " )");
+        } catch (Exception x) {
+            log.error(x.getMessage());
+        }
+    }
+
+    /**
+     * Sends key-value pairs from the given map to the topic as a MapMessage.
+     * 
+     * @param map Map of key-value pairs to be sent.
+     */
+    public void sendMapToTopic(Map<String, String> map) {
+        try {
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(jmsBrokerURL);
+            Connection connection = connectionFactory.createConnection();
+            connection.start();
+            Session session = connection.createSession(isJMSSessionTransacted, jmsAcknowledgmentMode);
+            Destination destination = session.createTopic(topicName);
+            MessageProducer producer = session.createProducer(destination);
+            producer.setDeliveryMode(jmsDeliveryMode);
+            MapMessage message = session.createMapMessage();
+
+            for (Map.Entry<String, String> entry : map.entrySet()) {
+                message.setObject(entry.getKey(), entry.getValue());
+            }
+
+            producer.send(message);
+            session.close();
+            connection.close();
+            log.info("sendMapToQueue( " + map + " )");
+        } catch (Exception x) {
+            log.error(x.getMessage());
+        }
+    }
+
 }
